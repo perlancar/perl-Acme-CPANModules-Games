ccce6940b3490e5822ee6f3ec328481a0faf2e1b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-13 10:44:21 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/Games.pm
@@ -0,0 +1,29 @@
+package Acme::CPANModules::Games;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use strict;
+use warnings;
+
+
+our $LIST = {
+    summary => 'Games on CPAN',
+    description => $text,
+    entries => [
+        {
+            module => 'Games::2048',
+        },
+        {
+            module => 'Games::FrozenBubble',
+        },
+        {
+            module => 'Games::Hangman',
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
